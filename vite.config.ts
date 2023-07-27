17d87a88d9bf7532ae56a91ad7795d795c1de2b1
Marcel Coelho <flasd2@gmail.com>
2023-07-27 19:49:59 +0200

    fix: fixes built files directory

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "BrazilianDates",
-      fileName: format => `${name}.${format}.js`,
+      fileName: format => `brazilian-dates.${format}.js`,
     },
   },
   test: {
